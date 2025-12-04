c11d00e080e87f590d9c3eaef9a9f3a90751299e
Hanane Mouder <hanane.mouder@docaposte.fr>
2025-12-05 00:22:50 +0100

    init auth api

---
+++ src/services/auth.api.ts
@@ -0,0 +1,51 @@
+import { API_URL } from "../config";
+
+export async function registerUser(
+  firstName: string,
+  lastName: string,
+  email: string,
+  password: string
+) {
+  const res = await fetch(`${API_URL}/auth/register`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ firstName, lastName, email, password }),
+  });
+
+  const data = await res.json().catch(() => ({}));
+
+  if (!res.ok) throw new Error(data.message || "Impossible de créer le compte");
+
+  return data;
+}
+
+export async function login(email: string, password: string) {
+  const res = await fetch(`${API_URL}/auth/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      origin: API_URL,
+    },
+    body: JSON.stringify({ email, password }),
+  });
+
+  const data = await res.json().catch(() => ({}));
+
+  if (!res.ok) {
+    throw new Error(data.message || "Identifiants incorrects");
+  }
+
+  return data;
+}
+
+export async function logout() {
+  const token = localStorage.getItem("UserToken");
+
+  await fetch(`${API_URL}auth/logout`, {
+    method: "POST",
+    headers: { Authorization: `Bearer ${token}` },
+  });
+
+  localStorage.removeItem("UserToken");
+  window.location.href = "/login";
+}
