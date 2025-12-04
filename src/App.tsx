682240098ed8b7b3e829ace784d71e26d772e171
Hanane Mouder <hanane.mouder@docaposte.fr>
2025-12-05 00:20:32 +0100

    fix navbar when user connected or not

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,35 @@
+import { SnackbarProvider } from "notistack";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import CreateShopPage from "./pages/shop/ShopManagment";
+import UserHome from "./pages/UserHome";
+import ProtectedRoute from "./routes/ProtectedRoutes";
 
-function App() {
+export default function App() {
   return (
-    <>
-      <Home />
-    </>
+    <SnackbarProvider maxSnack={3} autoHideDuration={4000}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/Home" element={<Home />} />
+
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <UserHome />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/ShopManagement"
+            element={
+              <ProtectedRoute>
+                <CreateShopPage />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </SnackbarProvider>
   );
 }
-
-export default App;
--- src/components/AppNavBar.tsx
+++ src/components/AppNavBar.tsx
@@ -1,46 +1,141 @@
-import { AppBar, Toolbar, Button, Typography, Box } from "@mui/material";
-import AnimatedButton from "./Button";
-import AuthModal from "../pages/AuthModal";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import LanguageIcon from "@mui/icons-material/Language";
+import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
+
+import {
+  AppBar,
+  Avatar,
+  Box,
+  IconButton,
+  InputAdornment,
+  TextField,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+
 import { useState } from "react";
+import UserMenuDrawer from "../layout/UserMenuDrawer";
+import AuthModal from "../pages/AuthModal";
+import AnimatedButton from "./Button";
 
 export default function AppNavbar() {
+  const token = localStorage.getItem("UserToken");
+  const firstname = localStorage.getItem("firstname") || "U";
   const [openAuth, setOpenAuth] = useState(false);
   const [authMode, setAuthMode] = useState<"login" | "register">("login");
+  const [openDrawer, setOpenDrawer] = useState(false);
+  const toggleDrawer = () => setOpenDrawer(!openDrawer);
 
   return (
     <>
       <AppBar
         position="static"
         elevation={0}
-        sx={{ bgcolor: "#fff", borderBottom: "1px solid #e5e5e5" }}
+        sx={{
+          bgcolor: "#fff",
+          borderBottom: "1px solid #e5e5e5",
+          py: 0.5,
+          pl: 10,
+        }}
       >
-        <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
-          <Typography variant="h6" fontWeight={700} sx={{ color: "#1e4fff" }}>
-            COLLECTOR<span>.shop</span>
+        <Toolbar
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            gap: 2,
+            px: 2,
+          }}
+        >
+          {/* LOGO */}
+          <Typography
+            variant="h5"
+            fontWeight={1000}
+            sx={{ color: "#1e4fff", whiteSpace: "nowrap" }}
+          >
+            COLLECTOR<span style={{ color: "#000" }}>.shop</span>
           </Typography>
 
-          <Box sx={{ display: "flex", gap: 3 }}>
-            <Button color="inherit">Browse</Button>
-            <Button color="inherit">Sell</Button>
-            <Button color="inherit">My Account</Button>
-            <Button color="inherit">Admin</Button>
-            <AnimatedButton
-              label="Sign In"
-              variant="outlined"
-              onClick={() => {
-                setAuthMode("login");
-                setOpenAuth(true);
+          {/* SEARCH BAR */}
+          <Box sx={{ flexGrow: 0.6 }}>
+            <TextField
+              fullWidth
+              placeholder="Rechercher une marque, un modèle..."
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <SearchIcon sx={{ color: "#1e4fff" }} />
+                  </InputAdornment>
+                ),
+                sx: {
+                  backgroundColor: "#f1f3f8",
+                  height: 50,
+                },
               }}
-            />{" "}
+              sx={{
+                "& .MuiInputBase-root": {
+                  fontSize: "16px",
+                  height: 50,
+                },
+              }}
+            />
+          </Box>
+
+          {/* RIGHT SECTION */}
+          <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
+            <FavoriteBorderIcon sx={{ color: "#1e4fff", fontSize: 30 }} />
+            <LanguageIcon sx={{ color: "#1e4fff", fontSize: 30 }} />
+
+            {/* Avatar (non clickable) */}
+            {token && (
+              <>
+                <Avatar sx={{ bgcolor: "#1e4fff", width: 45, height: 45 }}>
+                  {firstname[0].toUpperCase()}
+                </Avatar>
+
+                <IconButton
+                  disableRipple
+                  sx={{
+                    "&:focus": { outline: "none" },
+                    "&:focus-visible": { outline: "none" },
+                    "& .MuiTouchRipple-root": { display: "none" },
+                    "&:active": { outline: "none" },
+                    ml: 20,
+                  }}
+                  onClick={toggleDrawer}
+                >
+                  <MenuIcon sx={{ fontSize: 30, color: "#1e4fff" }} />
+                </IconButton>
+              </>
+            )}
+            {!token && (
+              <AnimatedButton
+                label="Se connecter"
+                variant="outlined"
+                width="auto"
+                onClick={() => {
+                  setAuthMode("login");
+                  setOpenAuth(true);
+                }}
+              />
+            )}
           </Box>
         </Toolbar>
       </AppBar>
+
       <AuthModal
         open={openAuth}
         onClose={() => setOpenAuth(false)}
         mode={authMode}
         setMode={setAuthMode}
-      />{" "}
+      />
+      {/* Drawer externe */}
+      <UserMenuDrawer
+        open={openDrawer}
+        onClose={toggleDrawer}
+        firstname={firstname}
+      />
     </>
   );
 }
