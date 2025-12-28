6acd5889a23f061dab53a0f6ee955ab4d731b4cd
Hanane Mouder <hanane.mouder@docaposte.fr>
2025-12-28 19:04:28 +0100

    configt to run test E2E in ci

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,11 +2,12 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: process.env.CYPRESS_baseUrl || "http://frontend-e2e:5173",
+    baseUrl: "http://localhost:5173",
     supportFile: false,
     video: false,
     chromeWebSecurity: false,
     defaultCommandTimeout: 20000,
     pageLoadTimeout: 60000,
+    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
   },
 });
