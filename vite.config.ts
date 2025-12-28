3f9d3f4dd3e25ee1221eeb55a761e26b5fdff8f4
Hanane Mouder <hanane.mouder@docaposte.fr>
2025-12-28 19:20:57 +0100

    allowHost to run test E2E in ci

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,6 @@ export default defineConfig({
   server: {
     host: "0.0.0.0",
     port: 5173,
-    allowedHosts: ["collector.shop"],
+    allowedHosts: ["collector.shop", "frontend-e2e", "localhost"],
   },
 });
